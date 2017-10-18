ce2fff8286993ac8a7692c028b87898ee8be7c4e
losiz17 <ripicea@gmail.com>
2017-10-18 21:24:48 +0900

    add faye-websocket eventmachine

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 # Gemfile
 source 'https://rubygems.org'
+ruby "2.4.2"
 
 gem 'http'
 gem 'json'
+gem 'faye-websocket'
+gem 'eventmachine'
--- main.rb
+++ main.rb
@@ -1,10 +1,43 @@
+# test.rb
 require 'http'
 require 'json'
+require 'eventmachine'
+require 'faye/websocket'
 
-response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
-    token: ENV['SLACK_API_TOKEN'],
-    channel: "#general",
-    text: "こんにちは！",
-    as_user: true,
+response = HTTP.post("https://slack.com/api/rtm.start", params: {
+    token: ENV['SLACK_API_TOKEN']
   })
-puts JSON.pretty_generate(JSON.parse(response.body))
+
+rc = JSON.parse(response.body)
+
+url = rc['url']
+
+EM.run do
+  # Web Socketインスタンスの立ち上げ
+  ws = Faye::WebSocket::Client.new(url)
+
+  #  接続が確立した時の処理
+ws.on :open do
+    p [:open]
+  end
+
+  ws.on :message do |event|
+    data = JSON.parse(event.data)
+    p [:message, data]
+
+    if data['text'] == 'こんばんは'
+      ws.send({
+        type: 'message',
+        text: "こんばんは <@#{data['user']}> さん",
+        channel: data['channel']
+        }.to_json)
+    end
+  end
+
+  ws.on :close do
+    p [:close, event.code]
+    ws = nil
+    EM.stop
+  end
+
+end
